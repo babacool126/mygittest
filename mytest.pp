e7b0f07fcd268654ff265a1f9842363896b6520a
babacool126 <babacool126@gmail.com>
2023-08-02 21:53:15 +0200

    aliasses tests

--- mytest.pp
+++ mytest.pp
@@ -5,3 +5,10 @@ This is a test
 This is a test
 This is a test
 This is a test
+
+This is another test
+This is another test
+This is another test
+This is another test
+This is another test
+This is another test
